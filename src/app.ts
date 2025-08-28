742c3b5e9dd29c18dbf0bb7bc502e3526de91167
isha-web1 <isha.web1@gmail.com>
2025-08-28 21:44:44 +0600

    create route,globalErrorHandler & refactore app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
-import express, { Application,  } from 'express'
+import express, { Application } from 'express';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 import cookieParser from 'cookie-parser';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 //parsers
@@ -8,13 +15,18 @@ app.use(express.json());
 
 app.use(cors({ origin: ['http://localhost:3000'] }));
 app.use(cookieParser())
-
-
+// application routes
+app.use('/api/v1', router);
 
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Welcome to the Apollo Gears API Service',
   });
 });
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
